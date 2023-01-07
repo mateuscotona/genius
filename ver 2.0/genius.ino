cdbb96a9cf42681abe61593c23271c81059108d2
mateuscotona <115283784+mateuscotona@users.noreply.github.com>
2023-01-07 12:52:21 -0300

    alterada o arquivo genens.uno

--- ver 2.0/genius.ino
+++ ver 2.0/genius.ino
@@ -88,6 +88,7 @@ void loop() {
 
 /* Função que sorteia a sequência da próxima rodada */
 void proximaRodada() {
+   //ooooooooooooooooooooooooo
   sequencia[rodada] = random(4);
   rodada = rodada + 1;
 }
